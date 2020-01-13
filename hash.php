33acad3bd0d3dbd7ee61eb7b2a632a08839117b7
Xin Liu <xinliu253@yahoo.com>
2020-01-12 20:05:39 -0500

    rehash

--- hash.php
+++ hash.php
@@ -1,18 +1,27 @@
 <?php
 $password = "xinliu1111";
-$hash = password_hash($password, PASSWORD_BCRYPT,array("cost"=>11));
+$hash = password_hash($password, PASSWORD_BCRYPT);
 
 
 echo $hash;
 $options = array("cost"=>11);
 
-if(password_verify($password, $hash)){
+if(password_verify("xinliu1111", $hash)){
 	echo "<br/><br/> Password is valid";
-}else{
-	echo "<br/><br/> Password is not valid";
-}
-if(password_needs_rehash($hash, PASSWORD_DEFAULT, $options)){
+	if(password_needs_rehash($hash, PASSWORD_DEFAULT, $options)){
 		$newHash = password_hash($password, PASSWORD_DEFAULT,$options);
 		echo "<br/><br/> New Hash: " . $newHash;
 	}
+}else{
+	echo "<br/><br/> Password is not valid";
+}
+
+
+
+if(password_verify("xinliu1111", $newHash)){
+	echo "<br/><br/> Password is valid";
+	
+}else{
+	echo "<br/><br/> Password is not valid";
+}
 ?>
